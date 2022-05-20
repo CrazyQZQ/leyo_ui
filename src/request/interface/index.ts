e7060682500dbc5ccf8c10bac64afd7ef156fa7c
CrazyQZQ <13186061107@163.com>
2022-05-20 17:23:44 +0800

    优化

--- src/request/interface/index.ts
+++ src/request/interface/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { AxiosInstance } from 'axios'
 import type { RequestInterceptors, RequestConfig, BaseResponseType } from './type'
-import { Toast } from 'vant'
+import { Toast,Dialog } from 'vant'
 import router from '@src/router'
 
 class configrequest {
@@ -29,7 +29,17 @@ class configrequest {
     // 所有请求的请求拦截器
     this.instance.interceptors.request.use(
       (config) => {
-        console.log('30',config)
+        // 加载动画
+        if (config.loading) {
+          Toast.loading({
+            message: '加载中...',
+            forbidClick: true
+          })
+        }
+        // 在此处添加请求头等，如添加 token
+        // if (store.state.token) {
+        // config.headers['Authorization'] = `Bearer ${store.state.token}`
+        // }
         return config
       },
       (err) => {
@@ -39,10 +49,31 @@ class configrequest {
     // 所有请求的响应拦截器
     this.instance.interceptors.response.use(
       (res) => {
-        const data = res.data
-        return data
+        Toast.clear()
+        // const data = res.data
+        // return data
+        if (res.code !== 0) {
+          // token 过期
+          if (res.code === 401) {
+            // 警告提示窗
+            return
+          }
+          if (res.code == 403) {
+            Dialog.alert({
+              title: '警告',
+              message: res.msg
+            }).then(() => {})
+            return
+          }
+          // 若后台返回错误值，此处返回对应错误对象，下面 error 就会接收
+          return Promise.reject(new Error(res.msg || 'Error'))
+        } else {
+          // 注意返回值
+          return response.data
+        }
       },
       (err) => {
+        Toast.clear()
         return err
       }
     )
