d473846bad758abec0f588334d902067fc7a6124
CrazyQZQ <13186061107@163.com>
2022-05-20 10:43:26 +0800

    router拦截

--- src/router/index.ts
+++ src/router/index.ts
@@ -83,5 +83,20 @@ const router = createRouter({
 	history: createWebHashHistory(),
 	routes
 })
-
+// 4. 你还可以监听路由拦截，比如权限验证。
+router.beforeEach((to, from, next) => {
+	// 1. 每个条件执行后都要跟上 next() 或 使用路由跳转 api 否则页面就会停留一动不动
+	// 2. 要合理的搭配条件语句，避免出现路由死循环。
+	const token = localStorage.get('token')
+	if (to.meta.auth) {
+		if (!token) {
+			return router.replace({
+				name: 'login'
+			})
+		}
+		next()
+	} else {
+		next()
+	}
+})
 export default router
