8c81c3cf4a01f22c78040d5eef4b5e59c4cafb53
CrazyQZQ <13186061107@163.com>
2022-05-24 16:00:38 +0800

    首页接口

---
+++ src/api/home.ts
@@ -0,0 +1,19 @@
+/**
+ * 查询banners
+ * @returns
+ */
+export const getBanners = () => {
+    return request.get({
+        url: '/system/home/banners'
+    })
+}
+
+/**
+ * 获取公告
+ * @returns
+ */
+export const getAnnouncement = () => {
+    return request.get({
+        url: '/system/home/announcement'
+    })
+}
